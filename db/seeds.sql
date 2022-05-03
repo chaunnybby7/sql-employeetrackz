a4c5712bc3f4829e88d931c27c5cbeb0dc320461
YiLin Ong <chauntelleinc@gmail.com>
2022-05-02 20:39:22 -0400

    added code to insert into department on seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Bob', 'The Builder', 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Trevor', 'Noah', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Johnny', 'Depp', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Mickey', 'Thompson', 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jane', 'Doe', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Amy', 'Winehouse', 5, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ronny', 'Chieng', 6, null);
+
+INSERT INTO department (department_name)
+VALUES ('Management');
+INSERT INTO department (department_name)
+VALUES ('Sales');
+INSERT INTO department (department_name)
+VALUES ('Accounting');
+INSERT INTO department (department_name)
+VALUES ('Reception');
+INSERT INTO department (department_name)
+VALUES ('Human Resources');
+
